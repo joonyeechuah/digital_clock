e98f1506655fcf589d97858f15f52dd28a640e41
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2015-10-01 09:40:51 -0500

    Initial Push
    With Seven Segment Library from Arduino

---
+++ digital_clock.ino
@@ -0,0 +1,17 @@
+// Arduino Clock
+//
+
+#include "SevSeg.h"
+
+#define d1 13
+#define a 2
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
