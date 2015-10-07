120d6cd695a2b5523d6d45060e1b3bb400649e5c
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2015-10-07 08:33:24 -0500

    Added ability to manually set hours
    Doesn't correctly define the pinouts for your connection! Doesn't do minutes, yet. Doesn't auto-increment.

--- digital_clock.ino
+++ digital_clock.ino
@@ -1,19 +1,44 @@
 // Arduino Clock
-//
 
 #include "SevSeg.h"
 
 #define d1 13
-#define a 2
+#define d2 12
+#define d3 11
+#define d4 10
+#define a 9
+#define b 8
+#define c 7
+#define d 6
+#define e 5
+#define f 4
+#define g 1
+#define dp 0
+#define HOUR_BUTTON 2
+
+volatile int hour = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(d1, OUTPUT);
-  pinMode(a, OUTPUT);
+  
+  pinMode(HOUR_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(HOUR_BUTTON), hourUp, RISING);
+
+  byte numDigits = 4;
+  byte digitPins[] = {2, 3, 4, 5};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CAODE, numDigits, digitPins, segmentPins);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(d1, LOW);
-  digitalWrite(a, HIGH);
+  sevseg.setNumber(hour * 100, 3);
+  refreshDisplay();
+}
+
+void hourUp() {
+  hour = hour + 1;
+  if (hour > 12) {
+    hour = 1;
+  }
 }
