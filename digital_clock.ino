52aede7bc06a8093d519c1efa51e865ce6079bfb
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2015-10-14 08:45:15 -0500

    Changed interrupt logic
    Interrupt was messing up the SevSeg.h library functions. I think.
    Anyway, it now uses event timer logic rather than interrupt logic. The
    tick() method will activate every TICK_LENGTH milliseconds

--- digital_clock.ino
+++ digital_clock.ino
@@ -14,31 +14,63 @@
 #define f 4
 #define g 1
 #define dp 0
-#define HOUR_BUTTON 2
+#define HOUR_BUTTON A0
+#define TICK_LENGTH 250
 
 SevSeg sevseg;
 
-volatile int hour = 1;
+int hour = 1;
+int ticks = 0;
+unsigned long previousMillis = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Bootup");
+  pinMode(HOUR_BUTTON, INPUT);
   
-  pinMode(HOUR_BUTTON, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(HOUR_BUTTON), hourUp, RISING);
-
   byte numDigits = 4;
   byte digitPins[] = {d1, d2, d3, d4};
   byte segmentPins[] = {a, b, c, d, e, f, g, dp};
   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  checkTick();
   sevseg.setNumber(hour * 100, 3);
-  sevseg.refreshDisplay();
+  sevseg.refreshDisplay(); 
+}
+
+void checkTick() {
+   unsigned long currentMillis = millis();
+ 
+   // How much time has passed, accounting for rollover with subtraction!
+   if ((unsigned long)(currentMillis - previousMillis) >= TICK_LENGTH) {
+      // It's time to do something!
+      tick();
+ 
+      // Use the snapshot to set track time until next event
+      previousMillis = currentMillis;
+   }
+}
+
+void tick() {
+  Serial.println("TICK!");
+  ticks = ticks + 1;
+  readHourButton();
+}
+
+void readHourButton() {
+  if (digitalRead(HOUR_BUTTON) == HIGH) {
+    hourUp();
+  }
 }
 
 void hourUp() {
+  Serial.print("Hour up: ");
+  Serial.println(hour);
   hour = hour + 1;
   if (hour > 12) {
     hour = 1;
